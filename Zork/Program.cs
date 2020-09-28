7fdafb8bcde752df0ab338c558d3986cb4eafe9e
spookyyy <christianorellana097@gmail.com>
2020-09-28 14:22:31 -0400

    Zork 3.1 - Object-Oriented Rooms
    Changed rooms to be object oriented, using the class "Room" to represent their data.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,7 +6,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -20,6 +20,7 @@ namespace Zork
 
             string outputString;
             Commands command = Commands.UNKNOWN;
+            InitializeRoomDescription();
 
             while (command != Commands.QUIT)
             {
@@ -34,7 +35,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = CurrentRoom.Description;
                         break;
 
                     case Commands.NORTH:
@@ -87,11 +88,26 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly string[,] Rooms = 
+        private static void InitializeRoomDescription()
         {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Forest", "North of House", "Clearing" }
+            Rooms[0, 0].Description = "You are on a rock-strewn trail."; //Rocky Trail
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; //South of House
+            Rooms[0, 2].Description = "You are at the top of the Great Canyuon on its south wall."; //Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you."; //Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door."; //West of House
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; //Behind House
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of House
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
+        }
+
+        private static readonly Room[,] Rooms = 
+        {
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
         //stuck with the array here because it's a simpler structure, and i believe it's faster
